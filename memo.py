952d1a75b030226bf91a777196a02689f0a2655c
hiroyuki_ishige <hiroyukiishige@hotmail.com>
2023-08-29 20:00:58 +0700

    Update

--- memo.py
+++ memo.py
@@ -1,7 +1,7 @@
 """
 multi page website: https://www.udemy.com/course/the-complete-web-development-bootcamp/learn/lecture/37349924#overview
 
-Project portfolit: https://www.udemy.com/course/the-complete-web-development-bootcamp/learn/lecture/37350000#overview
+Project portfolio: https://www.udemy.com/course/the-complete-web-development-bootcamp/learn/lecture/37350000#overview
 
 
 """
